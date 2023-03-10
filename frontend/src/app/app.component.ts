ac411f1481581091a4aa6759a89a445b0d0fa011
firdous alam <firdous.alam2058@gmail.com>
2023-03-10 17:24:55 +0530

    adding card using angular material

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
 import {MessageComponent} from './components/message.component'
-
-
 @Component({
   selector: 'app-root',
-  template: `
-            <mat-card>
-              <mat-card-content>
-                <mat-divider></mat-divider>
-                <section>
-                  <div class="example-label">Raised</div>
-                  <div class="example-button-row">
-                    <button mat-raised-button>Basic</button>
-                    <button mat-raised-button color="primary">Primary</button>
-                    <button mat-raised-button color="accent">Accent</button>
-                    <button mat-raised-button color="warn">Warn</button>
-                    <button mat-raised-button disabled>Disabled</button>
-                    <a mat-raised-button href="https://www.google.com/" target="_blank">Link</a>
-                  </div>
-                </section>
-                
-              </mat-card-content>
-            </mat-card>`,
+  template: `<messages></messages>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- frontend/src/app/components/message.component.ts
+++ frontend/src/app/components/message.component.ts
@@ -2,7 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector : 'messages',
-    template : 'this is my message component <div *ngFor="let message of messages">{{message.text}} By {{message.owner}}</div>'
+    template : `<div *ngFor="let message of messages">
+                    <mat-card style="margin:10px">
+                        
+                            <mat-card-title> {{message.owner}}</mat-card-title>
+                            <mat-card-content>{{message.text}} </mat-card-content>
+                            
+                       
+                    </mat-card>
+                </div>`
 })
 export class MessageComponent {
     messages = [{
